bf56d6fcdc2ef8e047b30016b19ee2de607e3ff1
Hec <me@xynova.com>
2025-09-01 02:08:05 +1000

    fix again

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,12 @@
 
 set -e
 
-# If the first argument is "bash", execute the command directly
-if [ "$1" = "bash" ]; then
+# If the first argument is "bash", check if it's bash -c
+if [ "$1" = "bash" ] && [ "$2" = "-c" ]; then
+    # For bash -c "command", execute the command through our CLI
+    exec python3 -m repomap_tool.cli $3
+elif [ "$1" = "bash" ]; then
+    # For other bash commands, execute directly
     exec "$@"
 else
     # Otherwise, run the repomap-tool CLI
