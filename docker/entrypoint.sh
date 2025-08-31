7e65e24e15c74355fd04d440d4f19b97bf705c57
Hec <me@xynova.com>
2025-09-01 02:04:53 +1000

    fixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,5 +10,7 @@ if [ "$1" = "bash" ]; then
     exec "$@"
 else
     # Otherwise, run the repomap-tool CLI
+    # This allows users to run: docker run ... repomap-tool:latest search /workspace "query"
+    # And it gets converted to: python3 -m repomap_tool.cli search /workspace "query"
     exec python3 -m repomap_tool.cli "$@"
 fi
